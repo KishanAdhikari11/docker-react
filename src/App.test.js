ed965561082f6b6fb158b8023d24c208a4d2a5d4
Kishan <adhikarykishan11@gmail.com>
2025-02-07 14:25:56 +0545

    fix: fixed test related isssues

--- src/App.test.js
+++ src/App.test.js
@@ -3,22 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  
+  // Check if "Learn React" link is in the document
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
